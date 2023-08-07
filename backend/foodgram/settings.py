00b8c608a0f164d3860dfd52f349964289fab122
Kirill Golovin <kirill.golovin.git@mail.ru>
2023-08-08 03:13:40 +0600

    api

---
+++ backend/foodgram/settings.py
@@ -0,0 +1,137 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+LOAD_INGR_PATH = BASE_DIR / "backend" / "data" / "ingredients.csv"
+
+SECRET_KEY = '!)$(#l6g*q7+o1id*l6$f(_tw!=%ver==pwr3yq$cpj^in&9a$'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'corsheaders',
+    'rest_framework.authtoken',
+    'api.apps.ApiConfig',
+    'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
+    'colorfield',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'foodgram.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'foodgram.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+AUTH_USER_MODEL = 'users.GourmetUser'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':
+    ['rest_framework.authentication.TokenAuthentication', ],
+
+    'DEFAULT_PERMISSION_CLASSES':
+    ['rest_framework.permissions.IsAuthenticatedOrReadOnly', ],
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'HIDE_USERS': False,
+    'PERMISSIONS': {
+        'recipe': ('api.permissions.IsAuthorStaffOrReadOnly,',),
+        'recipe_list': ('api.permissions.IsAuthorStaffOrReadOnly',),
+        'user': ('api.permissions.IsOwnerUserOrReadOnly',),
+        'user_list': ('api.permissions.IsOwnerUserOrReadOnly',),
+    },
+    'SERIALIZERS': {
+        'user': 'api.serializers.UserSerializer',
+        'user_list': 'api.serializers.UserSerializer',
+        'current_user': 'api.serializers.UserSerializer',
+        'user_create': 'api.serializers.UserSerializer',
+    },
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ backend/recipes/models.py
@@ -0,0 +1,163 @@
+from colorfield.fields import ColorField
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.db.models import CASCADE, UniqueConstraint
+from users.models import GourmetUser
+
+
+class Tag(models.Model):
+    """Тэги для рецептов.
+    Связано с моделью Recipe через М2М."""
+    name = models.CharField(
+        verbose_name='Тэг',
+        max_length=200,
+        unique=True,
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг тэга',
+        unique=True,
+        max_length=200
+    )
+    color = ColorField(
+        verbose_name='Цветовой HEX-код',
+        default='#FF0000',
+    )
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ('name', )
+
+    def __str__(self) -> str:
+        return f'{self.name} (цвет: {self.color})'
+
+
+class Recipe(models.Model):
+    """Модель для рецептов.
+    Связана с пользователем-автором GourmetUser через O2М.
+    Связана с количеством игридиентов IngredientQuantity через O2М.
+    Связана с пользователем Tags через M2М.
+    Связана с GourmetUser через M2М для добавления в избронное и покупки."""
+    author = models.ForeignKey(
+        GourmetUser,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+    name = models.CharField(
+        'Название блюда',
+        help_text='Введите название блюда',
+        max_length=200
+    )
+    text = models.TextField(
+        'Текст рецепта',
+        help_text='Введите текст рецепта'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления, мин.',
+        default=1,
+        validators=(
+            MinValueValidator(
+                1,
+                'Слишком быстро, так не бывает!'
+            ),
+            MaxValueValidator(
+                600,
+                'Слишком долго.'
+            ),
+        ),
+    )
+    image = models.ImageField(
+        'Изображение блюда',
+        upload_to='recipe_pictures/',
+        blank=True,
+        null=True,
+    )
+    create_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Тэг',
+    )
+    is_favorite = models.ManyToManyField(
+        GourmetUser,
+        verbose_name='Избранное',
+        related_name='favorites'
+    )
+    is_in_shopping_list = models.ManyToManyField(
+        GourmetUser,
+        verbose_name='Список покупок',
+        related_name='shopping_list'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-create_date']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+
+class Ingredient(models.Model):
+    """Ингридиенты для рецепта.
+    Связана с моделью IngredientQuantity через O2М."""
+    name = models.CharField(
+        verbose_name='Ингридиент',
+        max_length=200,
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единицы измерения',
+        max_length=200
+    )
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ('name', 'measurement_unit')
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}.'
+
+
+class IngredientQuantity(models.Model):
+    """Количество ингредиента в конкретном рецепте.
+    Связана с Recipe через O2M
+    Связана с Ingredient через М2М."""
+    recipe = models.ForeignKey(
+        verbose_name='В каких рецептах',
+        related_name='ingredient',
+        to=Recipe,
+        on_delete=CASCADE,
+    )
+    ingredients = models.ForeignKey(
+        verbose_name='Связанные ингредиенты',
+        related_name='recipe',
+        to=Ingredient,
+        on_delete=CASCADE,
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество',
+        default=1,
+        validators=(
+            MinValueValidator(
+                1, 'Слишком мало.'
+            ),
+            MaxValueValidator(
+                10000000, 'Слишком много для любых мер.'
+            ),
+        ),
+    )
+
+    class Meta:
+        ordering = ('recipe', )
+        constraints = (
+            UniqueConstraint(
+                fields=('recipe', 'ingredients', ),
+                name='\n%(app_label)s_%(class)s ingredient alredy added\n',
+            ),
+        )
