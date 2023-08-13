d309be5389a7586b9dd5cfa8676c9303b12be269
Kirill Golovin <kirill.golovin.git@mail.ru>
2023-08-13 16:14:28 +0600

    Рабочий пост запрос к рецептам

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,21 +1,12 @@
 from api.models import Tag, Recipe, RecipeIngredient, Ingredient
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.exceptions import ValidationError
 
 class TagSerializer(serializers.ModelSerializer):
     """Сериализатор для вывода тегов"""
     class Meta:
         model = Tag
         fields = '__all__'
-
-
-class RecipeIngredientSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source='ingredient.id')
-    name = serializers.CharField(source='ingredient.name')
-    measurement_unit = serializers.CharField(source='ingredient.measurement_unit')
-
-    class Meta:
-        model = RecipeIngredient
-        fields = ('id', 'name', 'measurement_unit', 'amount')
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -43,16 +34,140 @@ class RecipeSerializer(serializers.ModelSerializer):
 #         model = RecipeIngredient
 #         fields = ('id', 'amount')
 
-class RecipeCreateSerializer(serializers.ModelSerializer):
-    #ingredients = RecipeIngredientCreateSerializer(many=True)
+class IngredientSerializer(serializers.ModelSerializer): #работает
+    #"""Сериализатор для ингредиентов"""
+    class Meta:
+        model = Ingredient
+        fields = '__all__' #('name', 'measurement_unit')
+
+
+class IngredientAmountSerializer(serializers.ModelSerializer): #работает
+    #"""Сериализатор для связывания ингредиента с рецептом для записи""" 
+    id = serializers.PrimaryKeyRelatedField(
+        queryset=Ingredient.objects.all()
+    )
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ['id', 'amount']
+
+class RecipeIngredientSerializer(serializers.ModelSerializer): #работает
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.CharField(source='ingredient.name')
+    measurement_unit = serializers.CharField(source='ingredient.measurement_unit')
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+
+class RecipeReadSerializer(serializers.ModelSerializer):
+    """Сериалайзатор для GET-запроса"""
+    tags = TagSerializer(many=True)
+    ingredients = serializers.SerializerMethodField()
 
     class Meta:
         model = Recipe
-        fields = ('name', 'cooking_time', 'text', 'tags') #'ingredients')
+        fields = [
+            'id', 'tags',
+            'ingredients', 'name',
+            'text', 'cooking_time'
+        ]
 
-class IngredientSerializer(serializers.ModelSerializer):
-    """Сериализатор для ингредиентов"""
+    def get_ingredients(self, instance):
+        return RecipeIngredientSerializer(
+            instance.recipeingredient_set.all(),
+            many=True
+        ).data
+
+
+class RecipeCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор для добавления рецептов"""
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+    )
+    ingredients = IngredientAmountSerializer(many=True)
+    
     class Meta:
-        model = Ingredient
-        fields = '__all__'
-        read_only_fields = '__all__',
+        model = Recipe
+        fields = ['id', 'tags', 'ingredients', 'name',
+                  'text', 'cooking_time']
+
+    def validate_tags(self, tags):
+        if not tags:
+            raise ValidationError('Отсутствие тега')
+        validated_tags = set()
+        for tag in tags:
+            validated_tags.add(tag)
+        return validated_tags
+
+    def validate(self, data):
+        ingredients = data['ingredients']
+        if not ingredients:
+            raise ValidationError(
+                'Требуется выбрать хотя бы один ингредиент!',
+                code=status.HTTP_400_BAD_REQUEST,
+            )
+        know_ingredients = []
+        for ingredient in ingredients:
+            if ingredient['id'] in know_ingredients:
+                raise ValidationError(
+                    'Ингредиенты повторяются',
+                    code=status.HTTP_400_BAD_REQUEST
+                )
+            know_ingredients.append(ingredient['id'])
+        return data
+
+
+    def add_ingredients(self, ingredients, recipe):
+        RecipeIngredient.objects.bulk_create(
+            [RecipeIngredient(
+                recipe=recipe,
+                ingredient=ingredient['id'],
+                amount=ingredient['amount'],
+            ) for ingredient in ingredients]
+        )
+
+    def add_tags(self, tags, recipe):
+        for tag in tags:
+            recipe.tags.add(tag)
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags')
+        recipe = Recipe.objects.create(**validated_data)
+        self.add_tags(tags, recipe)
+        self.add_ingredients(ingredients, recipe)
+        return recipe
+
+    def update(self, instance, validated_data):
+        tags = validated_data.pop('tags')
+        ingredients = validated_data.pop('ingredients')
+        instance = super().update(instance, validated_data)
+        instance.tags.clear()
+        instance.tags.set(tags)
+        instance.ingredients.clear()
+        self.add_ingredients(recipe=instance,
+                             ingredients=ingredients)
+        instance.save()
+        return instance
+
+    def to_representation(self, recipe):
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeReadSerializer(recipe,
+                                    context=context).data
+
+
+class ShortRecipeSerializer(serializers.ModelSerializer):
+    """Сериализатор для рецептов в сокращённой форме"""
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'image',
+            'cooking_time'
+        )
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,8 +1,12 @@
 from djoser.views import UserViewSet
 from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework import status, viewsets
 
 from api.models import Tag, Recipe, Ingredient
-from api.serializers import TagSerializer, RecipeSerializer, RecipeCreateSerializer, IngredientSerializer
+from api.serializers import TagSerializer, ShortRecipeSerializer, RecipeCreateSerializer, IngredientSerializer, RecipeReadSerializer
 
 class CastomUserViewSet(UserViewSet):
     pass
@@ -13,17 +17,49 @@ class TagViewSet(ModelViewSet):
 
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeReadSerializer
 
     def get_serializer_class(self):
         if self.action == 'create': 
             return RecipeCreateSerializer
-        return RecipeSerializer
+        return RecipeReadSerializer
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Вьюсет для рецептов"""
+    queryset = Recipe.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return RecipeReadSerializer
+        return RecipeCreateSerializer
+
+    def add_recipe(self, model, user, pk):
+        if model.objects.filter(user=user, recipe__id=pk).exists():
+            return Response('Этот рецпт уже есть',
+                            status=status.HTTP_400_BAD_REQUEST)
+        recipe = get_object_or_404(Recipe, id=pk)
+        model.objects.create(user=user, recipe=recipe)
+        serializer = ShortRecipeSerializer(recipe)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete_recipe(self, model, user, pk):
+        obj = model.objects.filter(user=user, recipe__id=pk)
+        if obj.exists():
+            obj.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response('Такого рецепта уже нет',
+                        status=status.HTTP_400_BAD_REQUEST)
 
 class IngredientViewSet(ModelViewSet):
     """Вьюсет для ингредиентов"""
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+
+
